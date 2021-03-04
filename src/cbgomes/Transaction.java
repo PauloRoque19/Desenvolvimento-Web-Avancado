0b0456f99aa9021c2b02173e336b31d4e718307a
Paulo Roque <paulinhoroque@hotmail.com>
2021-03-03 21:43:40 -0300

    Revisão de Java 03/02

---
+++ src/cbgomes/Transaction.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package cbgomes;
+
+import java.time.LocalDateTime;
+
+import lombok.Getter;
+import lombok.Setter;
+/**
+ * @author cbgomes
+ *
+ */
+
+@Getter
+@Setter
+public class Transaction {
+
+	private Account account;
+	private Double value;
+	private LocalDateTime dateTimeTrasaction;
+	private String typeTransaction;
+}
